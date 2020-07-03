cae64a0177b23c2b6eb87d53598aedd731c79b74
rxdn <dotrar@protonmail.com>
2020-07-03 20:09:20 +0100

    Don't handle reducing

--- http/modmailupload.go
+++ http/modmailupload.go
@@ -2,28 +2,16 @@ package http
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"github.com/TicketsBot/logarchiver/config"
-	"github.com/TicketsBot/logarchiver/discord"
 	"github.com/gin-gonic/gin"
 	"github.com/minio/minio-go/v6"
 )
 
 func (s *Server) modmailUploadHandler(ctx *gin.Context) {
-	var messages []discord.Message
-
-	if err := ctx.BindJSON(&messages); err != nil {
-		ctx.JSON(400, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
-
-	// re-marshal to our own format
-	encoded, err := json.Marshal(&messages)
+	body, err := ctx.GetRawData()
 	if err != nil {
-		ctx.JSON(500, gin.H{
+		ctx.JSON(400, gin.H{
 			"message": err.Error(),
 		})
 		return
@@ -54,7 +42,7 @@ func (s *Server) modmailUploadHandler(ctx *gin.Context) {
 	name := fmt.Sprintf("%s/modmail/%s%s", guild, freePrefix, uuid)
 
 	// DigitalOcean does not support RetailUntilDate
-	if _, err := s.client.PutObject(config.Conf.S3.Bucket, name, bytes.NewReader(encoded), int64(len(encoded)), minio.PutObjectOptions{
+	if _, err := s.client.PutObject(config.Conf.S3.Bucket, name, bytes.NewReader(body), int64(len(body)), minio.PutObjectOptions{
 		ContentType:     "application/json",
 	}); err != nil {
 		ctx.JSON(500, gin.H{
