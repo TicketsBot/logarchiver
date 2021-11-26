4d78c720975c0a5e8f1a47250994b4ac0ed3b886
rxdn <29165304+rxdn@users.noreply.github.com>
2021-11-26 15:51:04 +0000

    Add Reduce function

---
+++ discord/reduce.go
@@ -0,0 +1,31 @@
+package discord
+
+import (
+	"github.com/rxdn/gdl/objects/channel/message"
+)
+
+func ReduceMessages(messages []message.Message) []Message {
+	reduced := make([]Message, len(messages))
+
+	for i, message := range messages {
+		reduced[i] = ReduceMessage(message)
+	}
+
+	return reduced
+}
+
+func ReduceMessage(message message.Message) Message {
+	return Message{
+		Author:      User{
+			Id:            message.Author.Id,
+			Username:      message.Author.Username,
+			Discriminator: message.Author.Discriminator,
+			Avatar:        message.Author.Avatar.String(),
+			Bot:           message.Author.Bot,
+		},
+		Content:     message.Content,
+		Timestamp:   message.Timestamp,
+		Embeds:      message.Embeds,
+		Attachments: message.Attachments,
+	}
+}
